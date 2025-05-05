c183ae262e9408fed3f948c7dc583142d06e2d75
jhfidelis <fidelis.henrique.jh@gmail.com>
2025-05-04 22:23:23 -0300

    Implementando requisições e respostas

---
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -0,0 +1,26 @@
+package br.com.alura.screenmatch.principal;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class PrincipalComBusca {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o filme para ser pesquisado:");
+        var busca = sc.nextLine();
+
+        String endereco = "http://www.omdbapi.com/?t=" + busca + "&apikey=5282043b";
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(endereco))
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+    }
+}
